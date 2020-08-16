66329380d993ca457d269cc105a7382752917783
Alluysios <arribaallu@gmail.com>
2020-08-16 08:26:05 -0600

    Updates

---
+++ precache-manifest.8d36f65c21c585dc743014f2b398b858.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e82824f4ef676948e080165ac10b2f",
+    "url": "/hero-academia/index.html"
+  },
+  {
+    "revision": "69f799f0608474dd581c",
+    "url": "/hero-academia/static/css/main.ec646652.chunk.css"
+  },
+  {
+    "revision": "158ca089e5a5980522d8",
+    "url": "/hero-academia/static/js/2.d7514396.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hero-academia/static/js/2.d7514396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f799f0608474dd581c",
+    "url": "/hero-academia/static/js/main.87c9a757.chunk.js"
+  },
+  {
+    "revision": "0329f7f14b3adb281fef",
+    "url": "/hero-academia/static/js/runtime-main.92168b92.js"
+  }
+]);
